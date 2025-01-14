d7f43e9fba7094190016691b2b85d45ba28c3c17
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-14 20:21:18 +0000

    waleko__claude-3-opus_b289__id_198

--- tests/test_examples/test_context/test_reader_future_result.py
+++ tests/test_examples/test_context/test_reader_future_result.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     # because we want to highlight `managed` in this example:
     managed_httpx = managed(_show_titles(3), _close)
     future_result = managed_httpx(
-        FutureResultE.from_value(httpx.AsyncClient(timeout=5)),
+        httpx.AsyncClient(timeout=5),
     )
     print(anyio.run(future_result.awaitable))  # noqa: WPS421
     # <IOResult: <Success: (
